ebb1684adc3a3388980c384243206bab3abcd9f4
GUSTAVO PROVENZANO DE SOUSA <gustavo.psousa3@senacsp.edu.br>
2026-09-11 22:01:16 -0300

    Subindo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuComponent } from './shared/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fintechcrud';
+  titulo:string = 'fintech CRUD';
+  nome:string = "Ana";
+
 }
---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./shared/menu/menu.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, MenuComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class AppComponent {
+    titulo = 'fintech CRUD';
+    nome = 'Ana';
+}
